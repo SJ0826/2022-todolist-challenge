eb71b1746b982e975b0113e4c17c8a301b56bbe3
sujin <ikosdu60@gmail.com>
2022-11-03 23:32:44 +0900

    [todo] feat: TudoHeader 추가

--- 2022-11-03/pages/tutolist-1.tsx
+++ 2022-11-03/pages/tutolist-1.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Tutolist1 = () => <div>투두리스트1</div>;
+import { TutoHeader } from '@ui/core/components/tuto';
 
-export default Tutolist1;
+const Tutolist1 = () => (
+  <div>
+    투두리스트1
+    <TutoHeader title="할일" />
+  </div>
+);
