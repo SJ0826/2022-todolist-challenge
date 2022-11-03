2904e2ca3310c9d088e4c02831dfcc67a4cd99bf
sujin <ikosdu60@gmail.com>
2022-11-04 00:04:15 +0900

    [todo] feat: TodoCreate 추가

--- 2022-11-03/pages/todolist1.tsx
+++ 2022-11-03/pages/todolist1.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 
-import { TodoHeader } from '@ui/components/todo';
+import { TodoHeader, TodoCreate, TodoHeader } from '@components/todo';
+
+import getDateString from '../lib/utils/getDateString';
+
+const { dateString, dayName } = getDateString();
 
 const Todolist1 = () => {
   const today = new Date();
@@ -10,11 +14,35 @@ const Todolist1 = () => {
     day: 'numeric',
   });
   const dayName = today.toLocaleString('ko-KR', { weekday: 'long' });
+  const [isOpenCreate, setIsOpenCreate] = useState(false);
+  const [createInput, setCreateInput] = useState('');
+
+  const onToggleIsOpenCreate = () => {
+    setIsOpenCreate((prev) => !prev);
+  };
+
+  const onChangeCreateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setCreateInput(value);
+  };
+
+  const onSubmitCreate = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsOpenCreate(false);
+    setCreateInput('');
+  };
+
   return (
     <div>
       <TodoHeader dateString={dateString} dayName={dayName} unDoneTaskLength={0} />
+      <TodoHeader dateString={dateString} dayName={dayName} unDoneTaskLength={0} />
+      <TodoCreate
+        isOpen={isOpenCreate}
+        onToggle={onToggleIsOpenCreate}
+        onChange={onChangeCreateInput}
+        onSubmit={onSubmitCreate}
+        value={createInput}
+      />
     </div>
   );
 };
-
-export default Todolist1;
