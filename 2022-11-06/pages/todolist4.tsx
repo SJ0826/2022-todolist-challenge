a28799069c38c332a5df0785c91d38599c610e1a
Sujin <ikosdu60@gmail.com>
2022-11-06 20:11:30 +0900

    [ui] feat: TodoList기능 추가

--- 2022-11-06/pages/todolist4.tsx
+++ 2022-11-06/pages/todolist4.tsx
@@ -1,30 +1,66 @@
 
 import TodoCreate from '@ui/components/todo/TodoCreate'
 import TodoHeader from '@ui/components/todo/TodoHeader'
-import { useState } from 'react'
+import TodoList from '@ui/components/todo/TodoList'
+import { ChangeEvent, FormEvent, useRef, useState } from 'react'
 import styled from 'styled-components'
 
+export interface TodoItemTypes {
+  id: number
+  text: string
+  done: boolean
+}
 const todolist4 = () => {
+  const [todos, setTodos] = useState<TodoItemTypes[]>([])
   // 날짜 업데이트
   const date = new Date()
   const today = date.toLocaleDateString('ko-kr', { year: 'numeric', month: 'long', day: 'numeric' })
   const dayName = date.toLocaleDateString('ko-kr', { weekday: 'long' })
 
   // Todo 할일 생성하기
-  const [isOpenCreate, setIsOpenCreate] = useState(true)
+  const nextId = useRef(0)
+  const [createInput, setCreateInput] = useState('')
+  const [isOpenCreate, setIsOpenCreate] = useState(false)
   const onToggleisOpenCreate = () => {
     setIsOpenCreate((prev) => !prev)
   }
+  const onChangeCreateInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    setCreateInput(value)
+  }
+
+  const onSubmitCreate = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault
+    nextId.current += 1
+    setTodos((prev) => [...prev, { id: nextId.current, text: createInput, done: false }])
+    setIsOpenCreate(false)
+    setCreateInput('')
+  }
+
+  // TodoList 만들기
+
+  const onToggleDone = (id: number) => {
+    setTodos((prev) => prev.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo)))
+  }
+  const onClickDelete = (id: number) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+  const onDoneTask = todos.length
   return (
-    <Container>
-      <TodoHeader today={today} dayName={dayName} unDoneTask={0} />
-      <TodoCreate isOpen={isOpenCreate} onToggle={onToggleisOpenCreate} />
-    </Container>
+    <>
+      <TodoHeader today={today} dayName={dayName} unDoneTask={onDoneTask} />
+      <TodoList todos={todos} onToggleDone={onToggleDone} onClickDelete={onClickDelete} />
+      <TodoCreate
+        isOpen={isOpenCreate}
+        onToggle={onToggleisOpenCreate}
+        onChange={onChangeCreateInput}
+        onSubmit={onSubmitCreate}
+        value={createInput}
+      />
+    </>
   )
 }
 
 export default todolist4
 
-const Container = styled.div`
-`
 
--- 2022-11-06/ui/components/todo/TodoCreate.tsx
+++ 2022-11-06/ui/components/todo/TodoCreate.tsx
@@ -1,58 +1,48 @@
+import React, { ChangeEvent, FormEvent, memo } from 'react'
 import styled, { css } from 'styled-components'
 
 interface Props {
   isOpen: boolean
   onToggle: () => void
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void
+  value: string
 }
-const TodoCreate = ({ isOpen, onToggle }: Props) => {
+const TodoCreate = ({ isOpen, onToggle, onSubmit, onChange, value }: Props) => {
   return (
-    <Container>
-      <CreateForm>
-        {isOpen && <CreateInput />}
-        <CreateButton isOpen={isOpen} onClick={onToggle}>
-          {isOpen ? '닫기' : '추가'}
-        </CreateButton>
-      </CreateForm>
-    </Container>
+    <Wrapper>
+      <CreateButton isOpen={isOpen} onClick={onToggle}>
+        {isOpen ? '닫기' : '추가'}
+      </CreateButton>
+      {isOpen && (
+        <Container>
+          <CreateForm onSubmit={onSubmit}>
+            <Input autoFocus placeholder="입력 후 Enter를 누르세요" onChange={onChange} value={value} />
+          </CreateForm>
+        </Container>
+      )}
+    </Wrapper>
   )
 }
-export default TodoCreate
 
-const Container = styled.div`
-  position: absolute;
-  width: 100%;
-  height: auto;
-  bottom: 0;
-  left: 0;
-`
+export default memo(TodoCreate)
 
-const CreateForm = styled.form`
-  position: absolute
-  width: 100%
-
-  background: #bcead5;
- 
-`
-
-const CreateInput = styled.input`
-  padding: 12px;
-  border-radius: 4px;
-  border: 1px solid #fff8ea;
-  width: 360px;
-  height: 50px;
-  outline: none;
-  font-size: 18px;
-  box-sizing: border-box;
-`
-
+const Wrapper = styled.div``
 const CreateButton = styled.button<{ isOpen: boolean }>`
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center; /* 가로 - 중앙으로 */
+  justify-content: flex-start; /* 세로 - 상단으로 */
   width: 60px;
-  height: 60px
-  right: 0
+  height: 50px;
+  padding: 0 20px;
   position: absolute;
+  right: 0px;
+  bottom: 0px;
   background-color: #594545;
   font-size: 15px;
   color: white;
+
   border: none;
   outline: none;
   cursor: pointer;
@@ -61,9 +51,34 @@ const CreateButton = styled.button<{ isOpen: boolean }>`
   ${(props) =>
     props.isOpen &&
     css`
-      color: #222;
       border: 1px solid #ddd;
       width: 60px;
-      height: 50px;
     `}
+`
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  bottom: 0;
+  left: 0;
+`
+
+const CreateForm = styled.form`
+  background: #f8f9fa;
+  // padding: 32px 32px 72px 32px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #fff8ea;
+`
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #fff8ea;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+  height: 75px;
 `
