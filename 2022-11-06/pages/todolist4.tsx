455e13aacb58a8375c7388d9f27330f9d9d99e09
Sujin <ikosdu60@gmail.com>
2022-11-06 16:18:02 +0900

    [ui] feat: TodoHeader 추가

--- 2022-11-06/pages/todolist4.tsx
+++ 2022-11-06/pages/todolist4.tsx
@@ -1,5 +1,17 @@
+
+import TodoHeader from '@ui/components/todo/TodoHeader'
+import styled from 'styled-components'
+
 const todolist4 = () => {
-return(<div>투두리스트 4탄</div>)
+return (
+  <Container>
+    <TodoHeader today='오늘 날짜가 업데이트 됩니다.' dayName='요일이 업데이트 됩니다.' unDoneTask={0} />
+  </Container>
+)
 }
 
 export default todolist4
+
+const Container = styled.div`
+`
+
---
+++ 2022-11-06/ui/components/todo/TodoHeader.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+interface Props {
+  today: string
+  dayName: string
+  unDoneTask: number
+}
+
+const TodoHeader = ({ today, dayName, unDoneTask }: Props) => {
+  return (
+    <Header>
+      <Container>
+        <Title>TODO LIST</Title>
+        <Today>{today}</Today>
+        <DayName>{dayName}</DayName>
+        <UnDonTask>할 일이 {unDoneTask}개 남았습니다.</UnDonTask>
+      </Container>
+    </Header>
+  )
+}
+
+export default TodoHeader
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  height: 200px;
+  background-color: #9ed5c5;
+`
+
+const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  font-size: 50px;
+  top: 24px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`
+
+const Container = styled.div`
+  padding-left: 10px;
+  font-size: 18px;
+  color: #594545;
+`
+
+const Today = styled.h3`
+  margin-bottom: 10px;
+`
+
+const DayName = styled.div``
+
+const UnDonTask = styled.div``
