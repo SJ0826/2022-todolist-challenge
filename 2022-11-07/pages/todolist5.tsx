8e25a84ac1f54d6bff97cc7bf05a7c475593b177
sujin <ikosdu60@gmail.com>
2022-11-07 18:59:35 +0900

    [todo] feat: TodoCreate 추가

--- 2022-11-07/pages/todolist5.tsx
+++ 2022-11-07/pages/todolist5.tsx
@@ -1,12 +1,37 @@
+import TodoCreate from '@ui/todo/TodoCreate'
 import TodoHeader from '@ui/todo/TodoHeader'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 const todolist4 = () => {
   const date = new Date()
   const today = date.toLocaleDateString('ko-kr', { year: 'numeric', month: 'long', day: 'numeric' })
   const dayName = date.toLocaleDateString('ko-kr', { weekday: 'long' })
+
+  const [isOpen, setIsOpen] = useState(true)
+  const [createInput, setCreateInput] = useState('')
+
+  const onCreateToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const onChangeCreate = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    setCreateInput(value)
+  }
+
+  const onSubmitCreate = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault
+  }
   return (
     <>
       <TodoHeader today={today} dayName={dayName} unDoneTask={0} />
+      <TodoCreate
+        isOpen={isOpen}
+        onToggle={onCreateToggle}
+        onChange={onChangeCreate}
+        onSubmit={onSubmitCreate}
+        value={createInput}
+      />
     </>
   )
 }
