ce717453252dc55e5b0517c872bac2c2245c6de9
sujin <ikosdu60@gmail.com>
2022-11-09 17:21:06 +0900

    [ui]: feat: TodoCreate 추가

--- 2022-11-09/pages/todolist7.tsx
+++ 2022-11-09/pages/todolist7.tsx
@@ -1,14 +1,57 @@
+import TodoCreate from '@ui/components/todo/TodoCreate'
 import TodoHeader from '@ui/components/todo/TodoHeader'
 import getDateString from 'lib/utils/getDateString'
+import { ChangeEvent, FormEvent, memo, useCallback, useRef, useState } from 'react'
+
+export interface TodoItemType {
+  id: number
+  text: string
+  done: boolean
+}
 
 const TodoList7 = () => {
   const { dateString, dayName } = getDateString()
 
+  const [isOpen, setIsOpen] = useState(true)
+  const [todos, setTodos] = useState<TodoItemType[]>([])
+  const [createInput, setCreateInput] = useState('')
+  const nextId = useRef(0)
+
+  const onCreateToggle = useCallback(() => {
+    setIsOpen(!isOpen)
+  }, [isOpen])
+
+  const onCreateSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault
+      nextId.current += 1
+      setTodos((prev) => [...prev, { id: nextId.current, text: createInput, done: false }])
+      setIsOpen(false)
+      setCreateInput('')
+    },
+    [nextId, setTodos, setIsOpen, setCreateInput],
+  )
+
+  const onCreateChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target
+      setCreateInput(value)
+    },
+    [setCreateInput],
+  )
+
   return (
     <>
       <TodoHeader today={dateString} dayName={dayName} unDoneTask={0} />
+      <TodoCreate
+        isOpen={isOpen}
+        onToggle={onCreateToggle}
+        onSubmit={onCreateSubmit}
+        onChange={onCreateChange}
+        value={createInput}
+      />
     </>
   )
 }
 
-export default TodoList7
+export default memo(TodoList7)
