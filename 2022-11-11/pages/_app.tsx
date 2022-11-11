34fcca564dfb15d3bb03e48bd681069339843838
sujin <ikosdu60@gmail.com>
2022-11-11 18:05:41 +0900

    [env] feat: nextjs 초기 설정 추가

---
+++ 2022-11-11/pages/_app.tsx
@@ -0,0 +1,21 @@
+import Layout from '@ui/components/layout/Layout'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from '../ui/core/GlobalStyles'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>투두 리스트</title>
+      </Head>
+      <GlobalStyles />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
+}
+
+export default MyApp
