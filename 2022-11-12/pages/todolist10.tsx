c7363e1fd4ea810e151b23dda9d73cddd389f258
Sujin <ikosdu60@gmail.com>
2022-11-12 16:38:16 +0900

    [todo] feat: TodoList 추가

--- 2022-11-12/pages/todolist10.tsx
+++ 2022-11-12/pages/todolist10.tsx
@@ -1,9 +1,10 @@
 import TodoCreate from '@ui/components/todo/TodoCreate'
 import TodoHeader from '@ui/components/todo/TodoHeader'
+import TodoList from '@ui/components/todo/TodoList'
 import getDateString from 'lib/utils/getDateString'
-import { ChangeEvent, FormEvent, memo, useCallback, useRef, useState } from 'react'
+import { ChangeEvent, FormEvent, memo, useCallback, useMemo, useRef, useState } from 'react'
 
-interface TodoItemType {
+export interface TodoItemType {
   id: number
   text: string
   done: boolean
@@ -14,6 +15,8 @@ const todolist10 = () => {
   const [todos, setTodos] = useState<TodoItemType[]>([])
   const nextId = useRef(0)
   const [inputValue, setInputValue] = useState('')
+
+  const unDoneTask = useMemo(() => todos.filter((todo) => !todo.done).length, [todos])
 
   const onCreateButton = useCallback(() => {
     setIsOpenButton(!isOpenButton)
@@ -37,9 +40,24 @@ const todolist10 = () => {
     },
     [inputValue],
   )
+
+  const onToggleDone = useCallback(
+    (id: number) => {
+      setTodos((prev) => prev.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo)))
+    },
+    [todos],
+  )
+
+  const onClickDelete = useCallback(
+    (id: number) => {
+      setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    },
+    [todos],
+  )
   return (
     <>
-      <TodoHeader today={dateString} dayName={dayName} unDoneTask={0} />
+      <TodoHeader today={dateString} dayName={dayName} unDoneTask={unDoneTask} />
+      <TodoList onToggleDone={onToggleDone} onClickDelete={onClickDelete} todos={todos} />
       <TodoCreate isOpen={isOpenButton} onToggle={onCreateButton} onChange={onCreateChange} onSubmit={onCreateSubmit} />
     </>
   )
