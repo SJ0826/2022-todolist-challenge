ecc1da95fe698a49ad942c09c129cbf6c9a06ef0
sujin <ikosdu60@gmail.com>
2022-11-18 17:22:56 +0900

    [env] feat: nextjs 초기 설정 추가

---
+++ 2022-11-18/pages/_app.tsx
@@ -0,0 +1,17 @@
+import GlobalStyles from '@ui/core/GlobalStyles'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>투두 리스트</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ 2022-11-18/pages/todolist13.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function todolist13() {
+  return <div>todolist13</div>
+}
